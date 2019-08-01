3abc900fc3b92a2569d8f09342a749a52ec6efc0
abdellahia <46005419+abdellahia@users.noreply.github.com>
2019-08-01 13:10:45 +0200

    Add files via upload

---
+++ Services/LogginService.cs
@@ -0,0 +1,37 @@
+﻿
+using launchpad_backend.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace launchpad_backend.Services
+{
+    public class LogginService
+    {
+        public static void InsertItem(SqlServerLoggingItem item)
+        {
+            using (LoggerContext ctx = new LoggerContext())
+            {
+                try
+                {
+                    var x = item.Api?? "";
+                    ctx.Database.ExecuteSqlCommand($"INSERT INTO BACKEND_LOG(TST,USERNAME,API,APP,ENDPOINT,QUERY,TIMING,STATUSCODE) " +
+                        $"VALUES (CURRENT_TIMESTAMP,@Username,@Api,@App,@Endpoint,@Query,@Timing,@StatusCode)",
+                        new SqlParameter("@Username", item.Username ?? ""),
+                        new SqlParameter("@Api", item.Api ?? ""),
+                        new SqlParameter("@App", item.App ?? ""),
+                        new SqlParameter("@Endpoint", item.Endpoint ?? ""),
+                        new SqlParameter("@Query", item.Query ?? ""),
+                        new SqlParameter("@Timing", item.Timing),
+                        new SqlParameter("@StatusCode", item.StatusCode));                   
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Failed to insert log entry: {ex}");
+                }
+
+            }
+        }
+    }
+}
