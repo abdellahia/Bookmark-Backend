5d66b828d2bf9d6ea9a505bb1d3f500907107437
abdellahia <46005419+abdellahia@users.noreply.github.com>
2019-08-01 13:06:03 +0200

    Add files via upload

---
+++ Helper/InputVariableHelper.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace launchpad_backend.Helper
+{
+    public class InputVariableHelper
+    {
+        public static void ValidateInputs()
+        {
+
+            string serverNode = GetServernode();
+            string database = GetDatabase();
+            string uid = GetUid();
+            string pwd = GetPwd();
+            string loggeruid = LoggerGetUid();
+            string loggerpwd = LoggerGetPwd();
+            string loggerdb = LoggerGetDb();
+            if (string.IsNullOrEmpty(serverNode))
+            {
+                Environment.SetEnvironmentVariable("SERVERNODE", "KOB04187.koehlerpaper.com");
+                Console.WriteLine("Variable SERVERNODE not provided. Using the default Test System KOB04187.koehlerpaper.com");
+            }
+            if (string.IsNullOrEmpty(database))
+            {
+                Environment.SetEnvironmentVariable("DB", "APP_DEV_LAUNCHPAD");
+                Console.WriteLine("Variable DB not provided. Using the default System APP_DEV_LAUNCHPAD");
+            }
+            if (string.IsNullOrEmpty(uid))
+            {
+                throw new Exception("Envionment Variable UID not provided");
+            }
+            if (string.IsNullOrEmpty(pwd))
+            {
+                throw new Exception("Envionment Variable PWD not provided");
+            }
+            if (string.IsNullOrEmpty(loggeruid))
+            {
+                throw new Exception("Envionment Variable LOGGERUID not provided");
+            }
+            if (string.IsNullOrEmpty(loggerpwd))
+            {
+                throw new Exception("Envionment Variable LOGGERPWD not provided");
+            }
+            if (string.IsNullOrEmpty(loggerdb))
+            {
+                throw new Exception("Envionment Variable LOGGERDB not provided");
+            }
+        }
+
+        public static string GetServernode()
+        {
+            return Environment.GetEnvironmentVariable("SERVERNODE");
+        }
+
+        public static string GetDatabase()
+        {
+            return Environment.GetEnvironmentVariable("DB");
+        }
+
+        public static string GetUid()
+        {
+            return Environment.GetEnvironmentVariable("UID");
+        }
+        public static string GetPwd()
+        {
+            return Environment.GetEnvironmentVariable("PWD");
+        }
+        public static string LoggerGetUid()
+        {
+            return Environment.GetEnvironmentVariable("LOGGERUID");
+        }
+        public static string LoggerGetPwd()
+        {
+            return Environment.GetEnvironmentVariable("LOGGERPWD");
+        }
+        public static string LoggerGetDb()
+        {
+            return Environment.GetEnvironmentVariable("LOGGERDB");
+        }
+    }
+}
---
+++ Helper/Keycloak.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+
+namespace launchpad_backend.Helper
+{
+    public class KeyCloak
+    {
+        public static List<string> GetRoles(IEnumerable<Claim> claims)
+        {
+            var result = new List<string>();
+            var obj = JObject.Parse(claims.FirstOrDefault(o => o.Type == "resource_access")?.Value);
+            foreach (var item in obj?.Children())
+            {
+                var token = obj?.SelectToken($"{item.Path}.roles");
+                result.AddRange(token?.Select(o => o.Value<string>()));
+            }
+            return result;
+        }
+
+        public static List<string> GetRealmRoles(IEnumerable<Claim> claims)
+        {
+            var result = new List<string>();
+            var obj = JObject.Parse(claims.FirstOrDefault(o => o.Type == "realm_access")?.Value);
+            var token = obj?.SelectToken($"roles");
+            result.AddRange(token?.Select(o => o.Value<string>()));
+            return result;
+        }
+
+        public static List<string> GetApps(IEnumerable<Claim> claims)
+        {
+            var result = new List<string>();
+            var obj = JObject.Parse(claims.FirstOrDefault(o => o.Type == "resource_access")?.Value);
+            foreach (var item in obj?.Children())
+            {
+                result.Add(item?.Path);
+            }
+            return result;
+        }
+
+        public static string GetProperty(IEnumerable<Claim> claims, string type)
+        {
+            return claims.FirstOrDefault(o => o.Type == type)?.Value;
+        }
+    }
+
+    public static class ClaimTypes
+    {
+        public const string Name = "name";
+        public const string Username = "preferred_username";
+        public const string Client = "azp";
+        public const string Givenname = System.Security.Claims.ClaimTypes.GivenName;
+        public const string Surname = System.Security.Claims.ClaimTypes.Surname;
+        public const string Email = System.Security.Claims.ClaimTypes.Email;
+    }
+}
+
